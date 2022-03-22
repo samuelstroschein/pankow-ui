5b82ff4a41e9bfbb9b5e66ffcbc7abdd8f2739cf
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-03-22 21:30:08 +0100

    add: colors
    Co-authored-by: Jannes Blobel <jannesblobel@users.noreply.github.com>

--- packages/tailwind-plugin/src/css.ts
+++ packages/tailwind-plugin/src/css.ts
@@ -3,9 +3,22 @@ import postcssJs from "postcss-js";
 import postcss from "postcss";
 
 /**
- * Parse PostCSS strings to a Css in JS object.
+ * Parses PostCSS strings to a Css in JS object.
  *
  * Read more here https://cssinjs.org/?v=v10.9.0
+ *
+ * @example
+ * const result = css(`
+ *  .button {
+ *    background-color:red;
+ *  }
+ * `)
+ * console.log(result)
+ * >> {
+ *  '.button': {
+ *    background-color: 'red'
+ *  }
+ * }
  */
 export function css(css: string): Record<string, string> {
   return postcssJs.objectify(postcss.parse(css));
--- packages/tailwind-plugin/src/index.ts
+++ packages/tailwind-plugin/src/index.ts
@@ -1,15 +1,20 @@
-import type { TailwindPluginFn } from "tailwindcss/plugin";
+import plugin from "tailwindcss/plugin";
 import * as components from "./components/index";
+import colors from "./colors/index";
 
-const plugin: TailwindPluginFn = ({ addComponents }) => {
-  console.log("pankow ui is activating...");
-  for (const name of Object.keys(components)) {
-    addComponents(
-      components[name as keyof typeof components]({ config: { styled: true } })
-    );
-  }
-  console.log("pankow ui setup complete");
-};
-
-// is commonjs syntax, tailwind requires commonjs
-module.exports = plugin;
+// tailwind requires commonjs
+// therefore, `module.exports` instead of `export plugin`
+module.exports = plugin(
+  ({ addComponents }) => {
+    console.log("pankow ui is activating...");
+    for (const name of Object.keys(components)) {
+      addComponents(
+        components[name as keyof typeof components]({
+          config: { styled: true },
+        })
+      );
+    }
+    console.log("pankow ui setup complete");
+  },
+  { theme: { extend: { colors } } }
+);
