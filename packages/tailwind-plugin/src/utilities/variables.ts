4462352f9d943e4ec70e7f47181d828359089599
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-03-23 01:10:00 +0100

    refactor: remove borders

--- packages/tailwind-plugin/src/utilities/variables.ts
+++ packages/tailwind-plugin/src/utilities/variables.ts
@@ -7,20 +7,7 @@ export function variables(args: { config: Config }) {
         --btn-focus-scale: 1;
         --animation-btn: 0;
         --animation-input: 0;
-    }
-    .tab-border-none {
-        --tab-border: 0px;
-    }
-    .tab-border {
-        --tab-border: 1px;
-    }
-    .tab-border-2 {
-        --tab-border: 2px;
-    }
-    .tab-border-3 {
-        --tab-border: 3px;
-    }
-  
-  
+    }  
+
   `);
 }
