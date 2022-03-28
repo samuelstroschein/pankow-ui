1212ebac4b0ca76ae90ab977807b19c99ab1a024
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-03-28 16:01:47 -0400

    add: export defaultConfig

--- packages/color-system/src/index.ts
+++ packages/color-system/src/index.ts
@@ -2,3 +2,4 @@ export { Color } from "./types/color";
 export { Config } from "./types/config";
 export { ParsedConfig } from "./types/parsedConfig";
 export { parseConfig } from "./parseConfig";
+export { defaultConfig } from "./defaultConfig";
