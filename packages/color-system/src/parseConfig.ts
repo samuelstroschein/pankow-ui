29df0c5db1fc6b59d8a4c9f875e16cf181e02d76
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-03-28 15:52:55 -0400

    add: interaction state colors

--- packages/color-system/src/parseConfig.ts
+++ packages/color-system/src/parseConfig.ts
@@ -29,7 +29,7 @@ function generateColors(config: Config) {
   const container = 100;
   const onContainer = 900;
 
-  const colors = {
+  const colors: ParsedConfig["colors"] = {
     primary: accentColors.primary[regular],
     "on-primary": white,
     "primary-container": accentColors.primary[container],
@@ -68,5 +68,42 @@ function generateColors(config: Config) {
     "on-error-container": semanticColors.error[onContainer],
   };
 
+  // add interaction state colors
+  // see https://m3.material.io/foundations/interaction-states
+  for (const [name, hex] of Object.entries(colors)) {
+    colors[`hover-${name}`] = colorShade(hex, 8);
+    colors[`focus-${name}`] = colorShade(hex, 12);
+    colors[`press-${name}`] = colorShade(hex, 12);
+    colors[`drag-${name}`] = colorShade(hex, 16);
+  }
+
   return colors;
 }
+
+/**
+ * Darkens or lightens a hex color.
+ */
+// @ts-ignore
+// taken from https://stackoverflow.com/a/62640342/16690118
+const colorShade = (col, amt) => {
+  col = col.replace(/^#/, "");
+  if (col.length === 3)
+    col = col[0] + col[0] + col[1] + col[1] + col[2] + col[2];
+
+  let [r, g, b] = col.match(/.{2}/g);
+  [r, g, b] = [
+    parseInt(r, 16) + amt,
+    parseInt(g, 16) + amt,
+    parseInt(b, 16) + amt,
+  ];
+
+  r = Math.max(Math.min(255, r), 0).toString(16);
+  g = Math.max(Math.min(255, g), 0).toString(16);
+  b = Math.max(Math.min(255, b), 0).toString(16);
+
+  const rr = (r.length < 2 ? "0" : "") + r;
+  const gg = (g.length < 2 ? "0" : "") + g;
+  const bb = (b.length < 2 ? "0" : "") + b;
+
+  return `#${rr}${gg}${bb}`;
+};
