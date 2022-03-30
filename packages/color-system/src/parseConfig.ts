954c743cc49b3525aaf9f5069d28f954cda6167b
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-03-30 14:10:51 -0400

    fix: alpha channel
    toHex8String needs to be called to include the
    alpha channel.

--- packages/color-system/src/parseConfig.ts
+++ packages/color-system/src/parseConfig.ts
@@ -64,33 +64,34 @@ function generateColors(config: Config) {
   // adding surface level colors
   // [level, opacity]
   for (const [level, opacity] of [
-    [100, 5],
-    [200, 8],
-    [300, 11],
-    [400, 12],
-    [500, 14],
+    [100, 0.05],
+    [200, 0.08],
+    [300, 0.11],
+    [400, 0.12],
+    [500, 0.14],
   ]) {
     // To follow the semantics of the other colors,
     // the "container" colors are added although they are the same color.
+    // using hex8 string to include alpha channel
     colors[`surface-${level}`] = new TinyColor(accentColors.primary[900])
       .setAlpha(opacity)
-      .toHexString();
+      .toHex8String();
     colors[`on-surface-${level}`] = colors["on-surface"];
     colors[`surface-${level}-container`] = new TinyColor(
       accentColors.primary[900]
     )
       .setAlpha(opacity)
-      .toHexString();
+      .toHex8String();
     colors[`on-surface-${level}-container`] = colors[`on-surface`];
   }
 
   // add interaction state colors
   // see https://m3.material.io/foundations/interaction-states
   for (const [name, hex] of Object.entries(colors)) {
-    colors[`hover-${name}`] = new TinyColor(hex).darken(8).toHexString();
-    colors[`focus-${name}`] = new TinyColor(hex).darken(12).toHexString();
-    colors[`press-${name}`] = new TinyColor(hex).darken(12).toHexString();
-    colors[`drag-${name}`] = new TinyColor(hex).darken(16).toHexString();
+    colors[`hover-${name}`] = new TinyColor(hex).darken(8).toHex8String();
+    colors[`focus-${name}`] = new TinyColor(hex).darken(12).toHex8String();
+    colors[`press-${name}`] = new TinyColor(hex).darken(12).toHex8String();
+    colors[`drag-${name}`] = new TinyColor(hex).darken(16).toHex8String();
 
     // no selected or activated colors because:
     //   "Unlike hover, focus, pressed, and dragged states
@@ -106,12 +107,12 @@ function generateColors(config: Config) {
   // different opacity values
   colors[`disabled-content`] = new TinyColor(colors["on-surface-100-container"])
     .darken(38)
-    .toHexString();
+    .toHex8String();
   colors[`disabled-container`] = new TinyColor(
     colors["on-surface-100-container"]
   )
     .darken(12)
-    .toHexString();
+    .toHex8String();
 
   return colors as ParsedConfig["colors"];
 }
