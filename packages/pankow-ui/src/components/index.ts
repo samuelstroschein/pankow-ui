0360ab5e626345703cea8fb93f39159cecc41d55
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2022-03-31 23:36:09 +0200

    Wip toast

--- packages/pankow-ui/src/components/index.ts
+++ packages/pankow-ui/src/components/index.ts
@@ -1,3 +1,4 @@
 export { alert } from "./alert";
 export { badge } from "./badge";
 export { button } from "./button";
+export { toast } from "./toast"
--- packages/pankow-ui/src/components/toast.ts
+++ packages/pankow-ui/src/components/toast.ts
@@ -2,34 +2,36 @@ import { css } from "../functions/css";
 import { ParsedConfig } from "../types/parsedConfig";
 import { forEachColorVariant } from "../functions/forEachColorVariant";
 
-export function button({ config }: { config: ParsedConfig }) {
+export function toast({ config }: { config: ParsedConfig }) {
   return css(`
    .toast{
-    flex items-center w-full max-w-xs p-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800
+    @apply flex bg-surface-100 items-center w-full max-w-xs p-4 ${config.borderRadius("base")} shadow ;
+
    }
+   
    .toast-icon{
-    inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-blue-500 bg-blue-100 rounded-lg dark:bg-blue-800 dark:text-blue-200
+    @apply inline-flex items-center justify-center flex-shrink-0 w-8 h-8  
+    ${config.borderRadius( "base")} ;
    }
   
-  .taost-content{
-    ml-3 text-sm font-normal
+  .toast-content{
+    @apply  ${config.fontSize("base")} font-medium mx-2;
   }
-
+  .toast-close{
+    @apply  ml-auto -mx-1.5 -my-1.5  ${config.borderRadius("base")} inline-flex h-8 w-8  p-1.5 
+  }
    ${forEachColorVariant(
      {
-       ...config.colorSystem.accentColors,
        ...config.colorSystem.semanticColors,
      },
      ({ name }) => `
-      .button-${name} { 
+      .toast-icon-${name} { 
         @apply
-          bg-${name}
-          text-on-${name}
-          hover:bg-hover-${name} hover:text-hover-on-${name}
-          focus:ring-focus-${name};
+        bg-${name}-container
+        text-on-${name}-container;
+          
       }
-
-      .button-outline-${name} {
+      .toast-close-${name} {
         @apply 
           text-${name}
           bg-transparent
@@ -37,12 +39,7 @@ export function button({ config }: { config: ParsedConfig }) {
           hover:bg-hover-${name} hover:text-hover-on-${name}
           focus:ring-focus-${name};
       }
-      .button-text-${name} {
-        @apply 
-          text-${name}
-          bg-transparent
-          hover:bg-hover-${name} hover:text-hover-on-${name};
-      }
+      
   `
    )}
    
