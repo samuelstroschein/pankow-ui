d0b17e2c105aad7b31e1b67bb61cb7890315d1ad
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-03-31 11:03:32 -0400

    refactor: manually define colors

--- packages/pankow-ui/src/functions/forEachColorVariant.ts
+++ packages/pankow-ui/src/functions/forEachColorVariant.ts
@@ -1,12 +1,11 @@
-import { ParsedConfig } from "../types/parsedConfig";
-import { css } from "./css";
+import { Color } from "@pankow-ui/color-system";
 
 /**
  * Each color variant that is supposed to lead to a dedicated component.
  *
  * @example
  *    ${forEachColorVariant(
- *     config.colorSystem.colors,
+ *     config.colorSystem.colors.accentColors,
  *     ({ name, bgColor, textColor }) =>
  *     .alert-${name} {
  *         @apply bg-${bgColor} text-on-${textColor};
@@ -14,15 +13,12 @@ import { css } from "./css";
  *  )}
  */
 export function forEachColorVariant(
-  colorSystem: ParsedConfig["colorSystem"],
+  colors: Record<string, Color>,
   callback: (value: { name: string }) => string
 ): string {
   // only accent and semantic colors are considered to by
   // component variants. Other, auto-generated colors
   // are not considered.
-  const colors = Object.keys({
-    ...colorSystem.accentColors,
-    ...colorSystem.semanticColors,
-  });
-  return colors.map((name) => callback({ name })).join("\n");
+  const colorArray = Object.keys(colors);
+  return colorArray.map((name) => callback({ name })).join("\n");
 }
